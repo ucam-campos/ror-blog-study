4ec0a9d3baec20228ffa0732fc5977d1d57ef9db
Lucas Seixas <seixas.lc@gmail.com>
2017-12-07 15:46:08 +0000

    v0.0.01 - refatorando em partials, forms e deletando comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments #relationship 1..N, 1 article has N comments
+    has_many :comments, dependent: :destroy #relationship 1..N, 1 article has N comments
     validates :title, presence: true,
                     length: { minimum: 5 } #ensure that all articles have a title that is at least five characters long
 end
